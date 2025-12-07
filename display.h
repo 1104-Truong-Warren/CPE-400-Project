8f2af30b9cf02cf3c9a53e88b63cd9ce716291db
1104-Truong-Warren <warrentruong2310@mail.com>
2025-12-06 17:55:16 -0800

    Created display.cpp and display.h
    Placeholder for now, getting the screen/window set up, will adapt to the node and edge classes later on.
    
    Right now does not work

---
+++ display.h
@@ -0,0 +1,24 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "node.h"
+#include "edge.h"
+
+using namespace std;
+
+class Display 
+{
+private:
+    sf::RenderWindow window;
+    sf::Font font;
+
+public:
+    Display(unsigned int width, unsigned int height);
+    void draw(const std::vector<Node>& nodes, const std::vector<Edge>& edges);
+    bool isOpen() const;
+    void pollEvents();
+};
+
+#endif
