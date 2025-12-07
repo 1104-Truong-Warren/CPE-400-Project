8f4da17ad0b7fabb9bb560e333d5c62704409b2b
MincedMeatTheFirst <112461281+1109-MaldonadoGomez-ChristopherAndre@users.noreply.github.com>
2025-12-06 18:17:53 -0800

    add constuctors to Node
    added constructors and macros to the Node class.

--- node.cpp
+++ node.cpp
@@ -1,1 +1,46 @@
 #include "node.h"
+
+Node::Node(){
+		id = '?'; //defualt node name
+		//Packet buffer[MAXBUFFERSIZE];
+		bufferCount = -1;
+		//initatlize the routing table with infinity
+		for(int i = 0; i < MAXNODES; i++ ){ 
+			for(int j = 0; j < MAXNODES; j++){
+				routingTable[i][j] = INF;
+			}
+		}
+		routingTableN = -1;
+		numEdges = -1; 
+}
+Node::Node(char id, int bufferCount, int routingTable[MAXNODES][MAXNODES], int routingTableN, Edge edges[MAXEDGES], int numEdges)
+	: id(id), 
+	bufferCount(bufferCount), 
+	routingTableN(routingTableN), 
+	numEdges(numEdges){
+
+	for(int i = 0; i < routingTableN; i++ ){ 
+		for(int j = 0; j < routingTableN; j++){
+			this->routingTable[i][j] = routingTable[i][j];
+		}
+	}
+	for(int i = 0; i < numEdges; i++){
+		this->edges[i] = edges[i];
+	}	
+}
+Node::Node(const Node& other)
+    : id(other.id),
+      bufferCount(other.bufferCount),
+      routingTableN(other.routingTableN),
+      numEdges(other.numEdges)
+{
+    for (int i = 0; i < routingTableN; i++) {
+        for (int j = 0; j < routingTableN; j++) {
+            this->routingTable[i][j] = other.routingTable[i][j];
+        }
+    }
+
+    for (int i = 0; i < numEdges; i++) {
+        this->edges[i] = other.edges[i];
+    }
+}
--- node.h
+++ node.h
@@ -1,18 +1,26 @@
 #ifndef NODE_H
 #define NODE_H
+
 #include <vector>
 #include "edge.h"
-#define MAXSIZE 50
+#define MAXNODES 50
+#define MAXEDGES 5
 #define MAXBUFFERSIZE 32
+#define INF 1000000
 class Node{
 	private:
 		char id;
-		int buffer[MAXBUFFERSIZE];
-		int routingTable[MAXSIZE][MAXSIZE];
-		int routingTableX;
-		int routingTAbleY;
-		Edge edges[];
+		//Packet buffer[MAXBUFFERSIZE];
+		int bufferCount; 
+		int routingTable[MAXNODES][MAXNODES];
+		int routingTableN;
+		Edge edges[MAXEDGES];
+		int numEdges; 
 	public:
 		Node();
+		Node(char id, int bufferCount, int routingTable[MAXNODES][MAXNODES], int routingTableN, Edge edges[MAXEDGES], int numEdges);
+		Node(const Node& rhs); 
+
+		
 };
 #endif
