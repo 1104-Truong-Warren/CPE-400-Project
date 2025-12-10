5122e5ca887f2a9f38eab4487a5647f5960e4b60
1104-Truong-Warren <warrentruong2310@mail.com>
2025-12-09 23:09:29 -0800

    Added more comments to display classes

--- display.cpp
+++ display.cpp
@@ -1,28 +1,31 @@
 #include "display.h"
 #include <iostream>
 
+// This creates the SFML window with given dimensions and title of the window
 Display::Display(unsigned int width, unsigned int height) : window(sf::VideoMode(width, height), "Network of routers in the Network Layer")
 {
+    // Sets the font to Arial
     if (!font.openFromFile("/System/Library/Fonts/Supplemental/Arial.ttf")) 
     {
         cerr << "Failed to load font!" << endl;
     }
 }
 
+// This function draws the network graph, takes in nodes and edges
 void Display::draw(const vector<Node>& nodes, const vector<Edge>& edges)
 {
     window.clear(sf::Color::Black); // Background color
 
-    // Draw edges
+    // Draws edges
     for (const auto& e : edges) 
     {
         window.draw(e.getLine());
     }
 
-    // Draw nodes and labels
+    // Draws nodes and labels
     for (const auto& n : nodes) 
     {
-        window.draw(n.getShape());
+        window.draw(n.getShape()); // Draws the node's shape such as a circle
 
         sf::Text text(font, string(1, n.getLabel()), 20);
         text.setFillColor(sf::Color::White);
@@ -35,11 +38,13 @@ void Display::draw(const vector<Node>& nodes, const vector<Edge>& edges)
     window.display();
 }
 
+// This function checks if the display is currently opened
 bool Display::isOpen() const 
 {
     return window.isOpen();
 }
 
+// This function handles the mouse clicks, keyboard input, and window resizing in the window
 void Display::pollEvents() 
 {
     while (const optional event = window.pollEvent()) 
